21a0ac06ed4a85e7f6d8dbca20eceaeb0f4065eb
Jacob Bashista <tgb.bashista@gmail.com>
2020-09-21 16:25:04 -0400

    Capitalization change in combo one

--- programs/comboOne.py
+++ programs/comboOne.py
@@ -1,5 +1,5 @@
 # M11 - Treadmill, M12 - Row Machine, M13 - Weight Machine
 
 def comboOne(robot, ev3):
-    ev3.speaker.say("You pressed Down, this button is not supported. DDDDDDDDDD")
+    ev3.speaker.say("You pressed down, this button is not supported. DDDDDDDDDD")
     pass
