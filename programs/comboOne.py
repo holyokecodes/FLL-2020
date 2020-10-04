65a3b43d7e6264130fc8e8ba4e19594f525e731a
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2020-10-04 16:40:13 -0400

    Working on combo one

--- programs/comboOne.py
+++ programs/comboOne.py
@@ -1,3 +1,11 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
 # M11 - Treadmill, M12 - Row Machine, M13 - Weight Machine
 
 def comboOne(robot, ev3, library):
@@ -5,7 +13,17 @@ def comboOne(robot, ev3, library):
     ## Use Library for line following to get to treadmill? Line follow to get back for sure
     robot.straight(library.inch_to_mm(8.5))
     robot.turn(-20)
-    library.line_follow_for_distance(p=-1, distance=library.inch_to_mm(58.4645))
-    # ev3.speaker.say("That was " + str(library.mm_to_inch(robot.distance())) + " Inches")
-    robot.straight(library.inch_to_mm(8.75))
+    sensor_stop = ColorSensor(Port.S1)
+    library.line_follow_for_distance(p=1, distance=library.inch_to_mm(48.4645), sensor_lf=sensor_stop)
+    ##ev3.speaker.say("That was " + str(library.mm_to_inch(robot.distance())) + " Inches")
+    robot.turn(180)
+    robot.straight(library.inch_to_mm(-12))
+    robot.settings(straight_speed=600)
+    robot.straight(library.inch_to_mm(-42))
+    robot.settings(straight_speed=150)
+
+    robot.straight(library.inch_to_mm(12))
+    library.line_follow_for_distance(p=1, distance=library.inch_to_mm(35), sensor_lf=sensor_stop)
+    robot.settings(straight_speed=600)
+    robot.straight(library.inch_to_mm(42))
     pass
