8716bffc7136c47c4dcb655e7fe510437de37c58
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2020-11-23 17:35:28 -0500

    Worked on slide

--- programs/comboThree.py
+++ programs/comboThree.py
@@ -43,3 +43,16 @@ def comboThree(robot, ev3, library, medium_motor):
     # Flip the light tire east, 
     # Flip the heavy tire south,
     pass
+
+    """# Drive to Bocchia    
+    robot.turn(-27)
+    robot.straight(library.inch_to_mm(9.5))
+    library.line_follow_for_distance(distance=library.inch_to_mm(inch=15), p=1.9, sensor_lf=sensor_l)
+    robot.turn(50)
+    robot.straight(library.inch_to_mm(20))
+    #Boccia Code goes here.
+    robot.turn(10)
+    robot.stop()
+    robot.settings(straight_speed = 900)
+    robot.straight(library.inch_to_mm(-60))
+    pass"""
--- programs/comboTwo.py
+++ programs/comboTwo.py
@@ -1,15 +1,11 @@
-#M08 - Boccia
-#
+# M03 - Slide
+
 def comboTwo(robot, ev3, library, medium_motor, sensor_l, sensor_r):
-    # Drive to Bocchia    
-    robot.turn(-27)
-    robot.straight(library.inch_to_mm(9.5))
-    library.line_follow_for_distance(distance=library.inch_to_mm(inch=15), p=1.9, sensor_lf=sensor_l)
-    robot.turn(50)
-    robot.straight(library.inch_to_mm(20))
-    #Boccia Code goes here.
-    robot.turn(10)
     robot.stop()
-    robot.settings(straight_speed = 900)
-    robot.straight(library.inch_to_mm(-60))
-    pass
+    robot.settings(straight_speed=33)
+    medium_motor.run_time(speed=200, time=2000, wait=False)
+    robot.straight(library.inch_to_mm(3.5))
+    robot.stop()
+    robot.settings(straight_speed=100)
+    robot.straight(library.inch_to_mm(-1.6))
+    robot.straight(library.inch_to_mm(1.8))
