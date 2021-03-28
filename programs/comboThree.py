93e299d4bf6ff1c077c43e5a5994cce1cc306423
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2021-03-28 19:50:31 -0400

    Combo Three is way closer to being done!

--- programs/comboThree.py
+++ programs/comboThree.py
@@ -11,10 +11,7 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 def comboThree(robot, ev3, library, medium_motor, sensor_l):
     
-    # settings(turn_acceleration)
-    # Go to bench,
-    # Run it over,
-    # Go to position where bucket can obtain the top piece of the bench,
+    
     # and take it.
     
     # robot.straight(library.inch_to_mm(3)) 
@@ -23,58 +20,62 @@ def comboThree(robot, ev3, library, medium_motor, sensor_l):
     # robot.turn(-140)
     #robot.straight(250)
     #robot.straight(-250)
+    
+    # Go to bench,
     robot.settings(turn_rate=57)
     robot.turn(-45)
     robot.straight(library.inch_to_mm(26))
     robot.turn(-120)
-    robot.straight(library.inch_to_mm(3)) #this forward hits the bench with the front of the attachment
-    #robot.turn(-30)
-    #robot.straight(library.inch_to_mm(12))
-    robot.straight(library.inch_to_mm(-1.5))
+
+    # Knock it over
+    robot.straight(library.inch_to_mm(3))
+    robot.straight(library.inch_to_mm(-2))
+
+    # Go to position where the backrest can be obtained
+
+    #Move around the bench
     robot.drive(100, -40)
     wait(1400)
     robot.stop(Stop.BRAKE)
+    
+    #Drive the robot aside of the backrest
     robot.straight(library.inch_to_mm(1))
-    robot.drive(110, 50)
+    
+    #Align parellel to backrest
+    robot.drive(110, 38.3)
     wait(1500)
     robot.stop(Stop.BRAKE)
-    robot.straight(library.inch_to_mm(7))
+
+    #Move tines even with backrest
+    robot.straight(library.inch_to_mm(5.5))
+    robot.stop(Stop.BRAKE)
+    robot.settings(turn_rate=50)
+    robot.turn(40)
+
+    #Throw the backrest across the room somehow
+    medium_motor.run_time(speed=1000, time=500)
+    wait(2000)
+    #robot.turn(-60)
+    
+    #Drop the cubes
+    #robot.straight(library.inch_to_mm(-2))
+    medium_motor.run_time(speed=-1000, time=750)
+    
+    #Go to home'
+    robot.turn(-15)
+    robot.straight(library.inch_to_mm(18))
+    
+
+'''
+    #Drives all the way toward the bench.
+    #robot.straight(library.inch_to_mm(7))
     robot.stop()
     #robot.turn(20)
     medium_motor.run_time(speed=500, time=2000)
     # medium_motor.run_time(speed=-10000, time=400)
     robot.turn(-35)
     robot.straight(library.inch_to_mm(-6.5))
-    medium_motor.run_time(speed=750, time=1100)
-    medium_motor.run_time(speed=-750, time=1100)
-
-    # Slide
-    # Get over there from tire,
-    # Knock down the first guy
-    # Get the second guy to the top,
-    # Second guy goes down,
-    # Use the attachment to put the guys on a tire
-    # Boccia
-    # Get over there from slide,
-    # Rotate boccia townards space where you can drop cubesw
-    # Push the cube into the square, if we have time use bucket to add more
-    # Bench
-    
-    
-    # Tires
-    # Drive over to the tires,
-    # Go back to base
-    # Flip the light tire east, 
-    # Flip the heavy tire south,
-    
-    # Drive to Bocchia    
-    # robot.turn(-27)
-    # robot.straight(library.inch_to_mm(9.5))
-    #library.line_follow_for_distance(distance=library.inch_to_mm(inch=15), p=1.9, sensor_lf=sensor_l)
-    # robot.turn(50)
-    # robot.straight(library.inch_to_mm(20))
-    # #Boccia Code goes here.
-    # robot.turn(10)
-    # robot.stop()
-    # robot.settings(straight_speed = 900)
-    # robot.straight(library.inch_to_mm(-60))
+    medium_motor.run_time(speed=-750, time=750)
+    #medium_motor.run_time(speed=750, time=7500)
+    #robot.driv
+'''
