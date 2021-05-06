7425553a1350dbec3e111f43e0b3c5da31ddcdb1
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2021-05-06 17:16:02 -0400

    Made combo one better
    Or, A push of frustration

--- programs/comboOne.py
+++ programs/comboOne.py
@@ -18,8 +18,8 @@ def comboOne(robot, ev3, library, left_motor, medium_motor, buttons):
     sensor_stop = ColorSensor(Port.S1) 
     #line follow
     library.line_follow_for_distance(p=1, distance=library.inch_to_mm(20.5), sensor_lf=sensor_stop, DRIVE_SPEED=150) #line follow to treadmill
-    robot.turn(90)
-    robot.turn(-90)
+    robot.turn(-70)
+    robot.turn(70)
     library.line_follow_for_distance(p=1, distance=library.inch_to_mm(29), sensor_lf=sensor_stop, DRIVE_SPEED=150) #line follow to treadmill
     wait(250)
     #turn to back onto treadmill
@@ -28,7 +28,7 @@ def comboOne(robot, ev3, library, left_motor, medium_motor, buttons):
     robot.straight(library.inch_to_mm(-12)) 
     robot.stop()
     #turn left motor to do the treamill
-    left_motor.run_target(speed = 600, target_angle=840) 
+    left_motor.run_target(speed = -600, target_angle=840) 
     #get off the treadmill
     robot.straight(library.inch_to_mm(13)) 
     # Drive to rowing machine
@@ -38,12 +38,12 @@ def comboOne(robot, ev3, library, left_motor, medium_motor, buttons):
     robot.straight(library.inch_to_mm(17.5))
     # turn to face the rowing machine
 
-    robot.turn(113) #if it breaks, set this to 114
+    robot.turn(110) #if it breaks, set this to 114
 
     # arrive at rowing machine
     robot.straight(library.inch_to_mm(1.6))
     # lower attachment to get the tire
-    medium_motor.run_time(speed=1000, time=1200)
+    medium_motor.run_time(speed=1000, time=2400)
     # backup to drag the tire to the target zone
     robot.straight(-132)
     robot.turn(20)
@@ -53,19 +53,18 @@ def comboOne(robot, ev3, library, left_motor, medium_motor, buttons):
     # Go to weight machine
     
 
-    # Lift weight
-
+  
     
-    robot.turn(-65-35)
+    robot.turn(-95)
     robot.straight(library.inch_to_mm(10))
-    robot.turn(-33) # it was -33
+    robot.turn(-40) # it was -33
     robot.straight(library.inch_to_mm(6))
     
-    medium_motor.run_time(speed=1000, time=5000, wait=False)
-    robot.drive(0,5)
-    wait(4000)
+    medium_motor.run_time(speed=1000, time=2750, wait=True)
+    #robot.drive(0,5)
+    wait(3000)
     robot.stop()
-    medium_motor.run_time(speed=-1000, time=3000)
+    medium_motor.run_time(speed=-1000, time=1500)
     robot.straight(library.inch_to_mm(-8))
     
     robot.drive(70,-70)
@@ -73,9 +72,11 @@ def comboOne(robot, ev3, library, left_motor, medium_motor, buttons):
     robot.stop()
     library.line_follow_for_distance(p=1, distance=library.inch_to_mm(40), sensor_lf=-2)
     robot.straight(library.inch_to_mm(2.5))
-    medium_motor.run_time(-1000, 2000, wait=False)
-    robot.turn(60)
-    robot.straight(library.inch_to_mm(40))
+    medium_motor.run_time(-1000, 1000, wait=False)
+    robot.turn(51)
+    robot.stop()
+    robot.settings(straight_speed=800)
+    robot.straight(library.inch_to_mm(44))
 
     # robot.settings(straight_speed=600)
     # robot.straight(library.inch_to_mm(36))
---
+++ programs/labelItTestCopy.py
@@ -0,0 +1,57 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+# M09 - Tires, M03 - Slide, M08 - Boccia, M04 - Bench
+
+#Facing toward Bench, back of robot on the wall, left wheel's edge is on the edge of the box.
+
+def comboThree(robot, ev3, library, medium_motor, sensor_l):
+    
+    # Go to bench,
+    robot.settings(turn_rate=57)
+    robot.turn(-45)
+    robot.straight(library.inch_to_mm(26))
+    robot.turn(-120)
+
+    # Knock it over
+    robot.straight(library.inch_to_mm(3))
+    robot.straight(library.inch_to_mm(-2))
+
+    # Go to position where the backrest can be obtained
+
+    #Move around the bench
+    robot.drive(100, -40)
+    wait(1400)
+    robot.stop(Stop.BRAKE)
+    
+    #Drive the robot aside of the backrest
+    robot.straight(library.inch_to_mm(1))
+    
+    #Align parellel to backrest
+    robot.drive(110, 38.3)
+    wait(1500)
+    robot.stop(Stop.BRAKE)
+
+    #Move tines even with backrest
+    robot.straight(library.inch_to_mm(5.5))
+    robot.stop(Stop.BRAKE)
+    robot.settings(turn_rate=50)
+    robot.turn(40)
+
+    #Throw the backrest across the room somehow
+    medium_motor.run_time(speed=-1000, time=650)
+    wait(2000)
+    #robot.turn(-60)
+    
+    #Drop the cubes
+    #robot.straight(library.inch_to_mm(-2))
+    medium_motor.run_time(speed=1000, time=500)
+    
+    #Go to home'
+    robot.turn(-85)
+    robot.straight(library.inch_to_mm(20))
+    
